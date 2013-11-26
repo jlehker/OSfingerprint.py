9b23762906f6c858e671075f2ab5ace4945466fa
rslavin <rocky.slavin@gmail.com>
2013-11-26 15:55:13 -0800

    added localhost to test.sh

--- test.sh
+++ test.sh
@@ -9,14 +9,15 @@ servers=( utsa.edu
         cs.utsa.edu 
         sefm.cs.utsa.edu 
         elk04.cs.utsa.edu 
-        slavin.info )
+        slavin.info 
+        localhost )
 
 echo "This script needs root access."
 echo "Please enter root's password when prompted."
 
 for i in "${servers[@]}"
 do
-    printf "\nOSfingerprint.py $i"
+    printf "\nOSfingerprint.py $i\n"
     sudo python OSfingerprint.py $i
 done
 
